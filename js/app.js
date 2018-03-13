c9deab53a14ca640b1e22a61be5d0508406aef12
meatbags <jxburrow@gmail.com>
2018-03-13 17:50:37 +1100

    fixing up site, rf modules

--- js/app.js
+++ js/app.js
@@ -1,78 +1,5 @@
-var stk =
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, {
-/******/ 				configurable: false,
-/******/ 				enumerable: true,
-/******/ 				get: getter
-/******/ 			});
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
 var App = {
-	init: function init() {
+	init : function() {
 		// mobile check
 
 		App.isMobile = window.mobileAndTabletcheck();
@@ -105,7 +32,7 @@ var App = {
 		}
 
 		// make long titles smaller
-		$(".nav__inner__title-main").each(function (i, e) {
+		$(".nav__inner__title-main").each(function(i,e) {
 			if ($(e).text().length > 20) {
 				$(e).addClass("font-smaller");
 			}
@@ -113,7 +40,7 @@ var App = {
 
 		// get video frames
 		if ($("iframe").length > 0) {
-			$("iframe").each(function (i, e) {
+			$("iframe").each(function(i, e){
 				var $f = $(e);
 
 				if ($f.attr("src").indexOf("vimeo") != -1) {
@@ -121,7 +48,7 @@ var App = {
 					$(".wrapper").prepend($(e));
 					$(e).css({
 						width: "100%"
-					});
+					})
 				}
 			});
 		}
@@ -129,34 +56,34 @@ var App = {
 		// get audio frames
 		App.checkAudio();
 
-		$(window).on('hashchange', function () {
+		$(window).on('hashchange', function() {
 			App.checkAudio();
 		});
 
 		// slick sliders
 		var $slider = $('.slick-slider').slick({
 			infinite: true,
-			slidesToShow: 1,
-			slidesToScroll: 1,
+		  slidesToShow: 1,
+		  slidesToScroll: 1,
 			dots: false,
 			arrows: false,
 			centerMode: true,
 			variableWidth: true,
-			responsive: [{
-				breakpoint: 960,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1,
-					infinite: true
-				}
-			}]
+		  	responsive: [{
+      			breakpoint: 960,
+      			settings: {
+        			slidesToShow: 1,
+        			slidesToScroll: 1,
+        			infinite: true
+      			}
+    		}]
 		});
 
-		$('.slick-slide').click(function () {
+		$('.slick-slide').click(function() {
 			// go to slide
 
 			$slider.slick('slickGoTo', parseInt($(this).data('slickIndex')));
-		});
+	  });
 
 		// animated text
 		App.setupAnimatedText();
@@ -165,10 +92,10 @@ var App = {
 		App.removeLoadingScreen();
 	},
 
-	setupAnimatedText: function setupAnimatedText() {
+	setupAnimatedText: function() {
 		// animated text
 
-		App.textIn = function (e) {
+		App.textIn = function(e) {
 			// animate in text
 
 			var target = e.find('.collection__name__inner');
@@ -190,51 +117,51 @@ var App = {
 
 			// animate in spans
 
-			target.find('.letter').css({ display: 'none' });
-			target.find('.letter').each(function (i, e) {
+			target.find('.letter').css({display: 'none'});
+			target.find('.letter').each(function(i, e) {
 				if ($(e).html() == breakKey) {
 					// show spaces, line-breaks immediately
 
-					if (length > threshold.min && length < threshold.max || length >= threshold.max && i % 4 == 0) {
+					if ((length > threshold.min && length < threshold.max) || (length >= threshold.max && i % 4 == 0)) {
 						$(e).html($(e).html().replace(breakKey, '<br />'));
 					} else {
 						$(e).html($(e).html().replace(breakKey, '&nbsp;'));
 					}
-					$(e).css({ display: 'inline' });
+					$(e).css({display: 'inline'});
 				} else {
 					// cut in other letters randomly
 
 					var t = Math.floor(Math.random() * 50 * length);
 
-					setTimeout(function () {
-						$(e).css({ display: 'inline' });
+					setTimeout(function(){
+						$(e).css({display: 'inline'});
 					}, t);
 				}
 			});
-		};
+		}
 
 		// destop/ mobile versions
 
 		if (!App.isMobile) {
 			// desktop -- on mouse
 
-			$('.collection__name').on('mouseenter', function () {
+			$('.collection__name').on('mouseenter', function() {
 				App.textIn($(this));
 			});
 		} else {
 			// mobile -- on scroll
 
-			$(document).on('scroll', function () {
+			$(document).on('scroll', function() {
 				var docTop = $(document).scrollTop();
 				var y = docTop + window.innerHeight / 2;
 				var docBottom = docTop + window.innerHeight;
 
-				$('.collection__name').each(function (i, e) {
+				$('.collection__name').each(function(i, e) {
 					var top = $(e).offset().top;
 					var centre = top + $(e).outerHeight() / 2;
 					var bottom = top + $(e).outerHeight();
 
-					if (y >= top && y <= bottom || centre >= docTop && centre <= docBottom) {
+					if ((y >= top && y <= bottom) || (centre >= docTop && centre <= docBottom)) {
 						// check flag
 
 						$parent = $(e).closest('.collection, .product');
@@ -257,12 +184,12 @@ var App = {
 		}
 	},
 
-	removeLoadingScreen: function removeLoadingScreen() {
+	removeLoadingScreen: function() {
 		$(".loading-screen").remove();
 		$("html").removeClass("freeze");
 	},
 
-	checkAudio: function checkAudio() {
+	checkAudio: function() {
 		// audio on home/collections
 		var autoplay = true;
 
@@ -275,7 +202,7 @@ var App = {
 				$('#jquery_jplayer_1').addClass('loaded-audio');
 
 				$("#jquery_jplayer_1").jPlayer({
-					ready: function ready(event) {
+					ready: function (event) {
 						$(this).jPlayer("setMedia", {
 							title: "",
 							mp3: audioTrackURLAlt
@@ -301,7 +228,7 @@ var App = {
 				});
 			}
 
-			$('.jp-pause, .jp-play').on('click', function () {
+			$('.jp-pause, .jp-play').on('click', function(){
 				$('.jp-pause, .jp-play').toggleClass('active');
 			});
 			$(".jp-jplayer, .jp-audio").removeClass("display-none");
@@ -309,7 +236,7 @@ var App = {
 		} else if (window.location.pathname === '/' || window.location.pathname === '') {
 			$('#jquery_jplayer_1').addClass('loaded-audio');
 			$("#jquery_jplayer_1").jPlayer({
-				ready: function ready(event) {
+				ready: function (event) {
 					$(this).jPlayer("setMedia", {
 						title: "",
 						mp3: audioTrackURLAlt
@@ -336,7 +263,7 @@ var App = {
 		}
 
 		// audio embedded in collections
-		$("p").each(function (i, e) {
+		$("p").each(function(i, e){
 			var $p, text;
 
 			$p = $(e);
@@ -346,7 +273,7 @@ var App = {
 				$(e).remove();
 
 				$("#jquery_jplayer_1").jPlayer({
-					ready: function ready(event) {
+					ready: function (event) {
 						$(this).jPlayer("setMedia", {
 							title: "",
 							mp3: text
@@ -372,29 +299,30 @@ var App = {
 				//$("#jquery_jplayer_1").jPlayer("play");
 				$(".jp-jplayer, .jp-audio").removeClass("display-none");
 			}
+
 		});
 	},
 
-	removeLandingPage: function removeLandingPage() {
+	removeLandingPage: function() {
 		var $landing = $('.landing-page, .landing-background, #landing-video');
 
 		$("html").removeClass("freeze");
 		$landing.addClass("hidden");
 		$('#landing-video').remove();
-		$(".landing-page__image").css({ display: "none" });
+		$(".landing-page__image").css({display: "none"});
 
-		setTimeout(function () {
-			$landing.remove();
+		setTimeout(function() {
+			$landing.remove()
 		}, 1000);
 
-		$(".menu-button, .nav").each(function (i, e) {
-			setTimeout(function () {
+		$(".menu-button, .nav").each(function(i, e){
+			setTimeout(function(){
 				$(e).removeClass("hidden");
 			}, (i + 1) * 200);
 		});
 
-		$(".collection.hidden").each(function (i, e) {
-			setTimeout(function () {
+		$(".collection.hidden").each(function(i, e){
+			setTimeout(function(){
 				$(e).removeClass("hidden");
 			}, 400 + Math.floor(Math.random() * 1000));
 		});
@@ -402,7 +330,7 @@ var App = {
 		window.location.hash = "collections";
 	},
 
-	toggleMenu: function toggleMenu() {
+	toggleMenu : function() {
 		if ($(".menu-button").hasClass("active")) {
 			$(".menu-button").removeClass("active");
 			$(".menu").removeClass("active");
@@ -412,7 +340,7 @@ var App = {
 		}
 	},
 
-	scroll: function scroll() {
+	scroll : function() {
 		if ($(".menu-button").hasClass("active")) {
 			App.toggleMenu();
 		}
@@ -429,17 +357,12 @@ var App = {
 	}
 };
 
-window.mobileAndTabletcheck = function () {
-	var check = false;
-	(function (a) {
-		if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
-	})(navigator.userAgent || navigator.vendor || window.opera);
-	return check;
+window.mobileAndTabletcheck = function() {
+  var check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
 };
 
-window.onload = function () {
+window.onload = function() {
 	App.init();
-};
-
-/***/ })
-/******/ ]);
+}
